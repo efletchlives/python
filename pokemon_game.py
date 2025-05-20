3a251ff04ca2fecdc6b01f6f20dcbbcc140c018d
Evan Fletcher <ehf21@pitt.edu>
2025-05-20 02:06:49 +0000

    yoyoyo

---
+++ pokemon_game.py
@@ -0,0 +1,11 @@
+# to access the class
+from pokedex import pokedex
+
+your_dex = pokedex()
+opp_dex = pokedex()
+
+print('welcome to pokemon!\nwhat would you like to do?"\n')
+print('1) catch pokemon\n2) battle opponent\n')
+num = input()
+
+
