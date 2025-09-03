84554954934c516c34f6de41931b78623e394c61
Evan Fletcher <ehf21@pitt.edu>
2025-09-03 02:02:49 +0000

    start hw 1

---
+++ week1/ps1.py
@@ -0,0 +1,24 @@
+# 1. Power Consumption Pricing
+# a) X would be the power used on a said day in a month in a year for many years. 
+
+# b) Y would be the price of electric per kwh on that day
+
+# c) Using previous discrete points several years in the past, 
+# you can train the model to predict power consumption in the future.
+
+# d) Choosing the level of regression (linear, polynomial, logarithmic) is a challenging part
+# because you want it to fit to the curve as best as possible especially for power consumption.
+# This typically decides what the power company will charge you for electric in the future.
+
+# 2. Identifying different types of cars in photos
+# a) X would be the photos of certain types of cars in them
+
+# b) Y would be a string of what brand car is in the photo
+
+# c) I would take photos from the internet of the cars on the road and then create the output 
+# by looking at the photos and writing down which brand it is (Toyota, Honda, Mercedes, BMW)
+
+# d) Since there is a lot of variation within car brands, I would need an emormous dataset.
+
+# 3) Basic Operations
+print("hello")
