18c5c55f050d3052960cb71dd8c94d44a382330e
Evan Fletcher <ehf21@pitt.edu>
2025-09-05 05:05:41 +0000

    getting close to done hw 1

--- week1/ps1.py
+++ week1/ps1.py
@@ -21,4 +21,56 @@
 # d) Since there is a lot of variation within car brands, I would need an emormous dataset.
 
 # 3) Basic Operations
-print("hello")
+import numpy as np
+
+# a)
+mean = 2.4
+std_dev = 0.75
+x = mean + std_dev * np.random.randn(1000000,1)
+
+# b)
+z = -2 + (1 + 2) * np.random.rand(1000000,1)
+
+# c)
+x_hist = np.histogram(x) # since x is already normalized
+
+
+# d)
+x_1 = x
+for i in range(np.size(x)):
+    x_1[i] = x[i] + 2
+    # figure out how to time operations
+
+# e)
+x_2 = x
+add_vector = np.ones(1000000) * 2
+x_2 = x + add_vector # using vector adding instead
+
+
+# 4) Linear Algebra
+
+# a)
+A = np.array([2,10,8],[3,5,2],[6,4,4])
+# find min value in each column, max value in each row, smallest value in A,
+# vector that contains sum of each row in A, sum of all elements in A,
+# compute matrix B whose size is the same as A but elements are the square of the corresponding elements in A
+
+# b)
+A = np.array([2,5,-2],[2,6,4],[6,8,18])
+b = np.array([12],[6],[15])
+# solve the system of equations
+
+# c)
+# L1-norm and L2-norm for vectors x1 and x2
+
+
+
+# 5) Splitting Data
+
+
+
+
+
+
+
+
