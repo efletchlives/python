7fca5cd28b3be24bb9d7ddb01878036d2f68a68e
Evan Fletcher <ehf21@pitt.edu>
2025-09-12 01:17:43 +0000

    finished 1.

--- week2/computeCost.py
+++ week2/computeCost.py
@@ -1,4 +1,16 @@
+import numpy as np
+
 def computeCost(X, y, θ):
-    for i in range(0,len(X)):
-        J = []
+    
+    # add X0 to the X matrix (maybe add later)
+    # X = np.vstack((ones_vec, X))
+
+    θ = np.transpose(θ)
+    Xθ = np.matmul(X,θ) # find h(x)
+    err = y - Xθ # find error
+    #  m x 1  m x 1
+    J_matrix =  np.matmul(np.transpose(err),err) # find variance
+    #        1 x m           m x 1
+
+    J = J_matrix.item()
     return J
--- week2/ps2.py
+++ week2/ps2.py
@@ -1,18 +1,47 @@
+# modules 
+import numpy as np
+
+# functions
 import computeCost
 import gradientDescent
 import normalEqn
 # θ = theta
 
-# import csv data
+# toy data set
+X = np.array([[1,0,1],[1,1,1.5],[1,2,4],[1,3,2]]) # 4x3
+y = np.array([[1.5],[4],[8.5],[8.5]]) # 4x1
+
+# theta (i)
+θ = np.array([[0.5,2,1]]) # 3x1
+J = computeCost.computeCost(X, y, θ)
+print('J(i):',J)
+
+# theta (ii)
+θ = np.array([[3,-1.5,-4]]) # 3x1
+J = computeCost.computeCost(X, y, θ)
+print('J(ii):',J)
+
+# theta (iii)
+θ = np.array([[0.5,1,2]]) # 3x1
+J = computeCost.computeCost(X, y, θ)
+print('J(iii):',J)
+
+# # compute cost function
+# J = computeCost(X, y, θ)
+
+# # compute gradient descent solution to linear regression
+# [θ, J] = gradientDescent(X_train, y_train, alpha, iters)
+
+# # compute closed-form solution to linear regression using normal equation
+# [θ] = normalEqn(X_train, y_train) 
 
 
-# compute cost function
-J = computeCost(X, y, θ)
+# 4.
+# import csv data for linear regression with one variable
+# X = np.loadtxt('hw2_data1.csv', usecols=0)
+# y = np.loadtxt('hw2_data1.csv', usecols=1)
 
-# compute gradient descent solution to linear regression
-[θ, J] = gradientDescent(X_train, y_train, alpha, iters)
-
-# compute closed-form solution to linear regression using normal equation
-[θ] = normalEqn(X_train, y_train) 
-
-
+# 5.
+# import csv data for linear regression with multiple variables 
+# X = np.loadtxt('hw2_data3.csv',usecols=(0,1))
+# y = np.loadtxt('hw2_data3.csv',usecols=2)
