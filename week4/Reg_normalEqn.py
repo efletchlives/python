5547c99034fb77e8791fb26bb9872aad5753527b
Evan Fletcher <ehf21@pitt.edu>
2025-09-29 01:40:12 +0000

    create a.

---
+++ week4/Reg_normalEqn.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def Reg_normalEqn(X_train, y_train, 𝜆):
+    nothing = []
+    return nothing
---
+++ week4/ps4.py
@@ -0,0 +1,13 @@
+import scipy.io
+
+# functions
+import Reg_normalEqn # computes closed form solution to linear regression
+
+
+# loading in data files
+data1 = scipy.io.loadmat('/workspaces/python/week4/hw4_data1.mat')
+data2 = scipy.io.loadmat('/workspaces/python/week4/hw4_data2.mat')
+data3 = scipy.io.loadmat('/workspaces/python/week4/hw4_data3.mat')
+
+
+
