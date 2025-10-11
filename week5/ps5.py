c524c3511abf9c8ca15bde3e11a24d758d117ffb
Evan Fletcher <ehf21@pitt.edu>
2025-10-11 06:01:18 +0000

    yoyoyo

--- week5/ps5.py
+++ week5/ps5.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib as plot
 import scipy.io
+from sklearn.metrics import accuracy_score
 
-data3 = scipy.io.loadmat('hw4_data3.mat')
+import weightedKNN
+
+
+data3 = scipy.io.loadmat('/workspaces/python/week5/input/hw4_data3.mat')
 X_train = data3['X_train']
 y_train = data3['y_train']
 X_test = data3['X_test']
@@ -11,4 +15,8 @@ y_test = data3['y_test']
 
 sigma = np.array([0.01,0.07,0.15,2,4])
 y_predict = weightedKNN.weightedKNN(X_train, y_train, X_test, sigma)
+
+accuracy = accuracy_score(y_test, y_predict)
+print('weighted KNN accuracy using Euclidean distance:',accuracy)
+
 
