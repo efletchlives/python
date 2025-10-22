80114d2e7c2e06acdc70d476f68036e441460810
Evan Fletcher <ehf21@pitt.edu>
2025-10-22 01:47:41 +0000

    finished coding!

--- power/per_unit_system.py
+++ power/per_unit_system.py
@@ -102,9 +102,16 @@ V4_pu = pu_functions.calc_vpu("V4_pu", V5_pu, I6_pu, Z_Ln2pu)
 
 I_Ld1 = V4_pu/Z_Ld1pu
 I3_pu = I6_pu + I_Ld1
-V3_pu = pu_functions.calc_vpu("V3_pu", V4_pu, I3_pu, x_T2pu)
+
+V3_pu = V4_pu + I3_pu * 1j * x_T2pu
+V3_pu = pu_functions.XFMR_phase(V3_pu, "Δ-Y")
+print(f"V3_pu = {round(np.abs(V3_pu),3)} < {round(np.angle(V3_pu, deg=True),3)}° pu")
+
 V2_pu = pu_functions.calc_vpu("V2_pu", V3_pu, I3_pu, Z_Ln1pu)
-V1_pu = pu_functions.calc_vpu("V1_pu", V2_pu, I3_pu, x_T1pu)
+
+V1_pu = V3_pu + I3_pu * 1j * x_T1pu
+V1_pu = pu_functions.XFMR_phase(V1_pu, "Y-Δ")
+print(f"V1_pu = {round(np.abs(V1_pu),3)} < {round(np.angle(V1_pu, deg=True),3)}° pu")
 
 print("per unit currents:")
 pu_functions.calc_ipu("I6_pu",I6_pu)
@@ -132,8 +139,7 @@ I3_actual = pu_functions.calc_actuali("I3",I_base2, I3_pu)
 I2_actual = pu_functions.calc_actuali("I2",I_base2, I3_pu)
 I1_actual = pu_functions.calc_actuali("I1",I_base1, I3_pu)
 
-
-
+# ----------------------------------------------------------------------------
 # ------------------------------- 5 ------------------------------------
 # employ a pf correction solution
 
@@ -171,9 +177,16 @@ V5_pu = pu_functions.calc_vpu("V5_pu", V6_pu, I6_pu, x_T3pu)
 V4_pu = pu_functions.calc_vpu("V4_pu", V5_pu, I6_pu, Z_Ln2pu)
 I_Ld1 = V4_pu / Z_Ld1pu
 I3_pu = I6_pu + I_Ld1
-V3_pu = pu_functions.calc_vpu("V3_pu", V4_pu, I3_pu, x_T2pu)
+
+V3_pu = V4_pu + I3_pu * 1j * x_T2pu
+V3_pu = pu_functions.XFMR_phase(V3_pu, "Δ-Y")
+print(f"V3_pu = {round(np.abs(V3_pu),3)} < {round(np.angle(V3_pu, deg=True),3)}° pu")
+
 V2_pu = pu_functions.calc_vpu("V2_pu", V3_pu, I3_pu, Z_Ln1pu)
-V1_pu = pu_functions.calc_vpu("V1_pu", V2_pu, I3_pu, x_T1pu)
+
+V1_pu = V3_pu + I3_pu * 1j * x_T1pu
+V1_pu = pu_functions.XFMR_phase(V1_pu, "Y-Δ")
+print(f"V1_pu = {round(np.abs(V1_pu),3)} < {round(np.angle(V1_pu, deg=True),3)}° pu")
 
 print("\nper unit currents:")
 pu_functions.calc_ipu("I6_pu", I6_pu)
@@ -186,7 +199,7 @@ pu_functions.calc_ipu("I1_pu", I3_pu)
 print('\nactual voltages:')
 V6_actual = pu_functions.calc_actualv("V6", 4.16e3, V6_pu)
 V5_actual = pu_functions.calc_actualv("V5", 12.47e3, V5_pu)
-V4_actual = pu_functions.calc_actualv("V4", 230e3, V4_pu)
+V4_actual = pu_functions.calc_actualv("V4", 12.47e3, V4_pu)
 V3_actual = pu_functions.calc_actualv("V3", 230e3, V3_pu)
 V2_actual = pu_functions.calc_actualv("V2", 230e3, V2_pu)
 V1_actual = pu_functions.calc_actualv("V1", 20e3, V1_pu)
@@ -226,9 +239,18 @@ V5_pu = pu_functions.calc_vpu("V5_pu", V6_pu, I6_pu, x_T3pu)
 V4_pu = pu_functions.calc_vpu("V4_pu", V5_pu, I6_pu, Z_Ln2pu)
 I_Ld1 = V4_pu / Z_Ld1pu
 I3_pu = I6_pu + I_Ld1
-V3_pu = pu_functions.calc_vpu("V3_pu", V4_pu, I3_pu, x_T2pu)
+
+
+V3_pu = V4_pu + I3_pu * 1j * x_T2pu
+V3_pu = pu_functions.XFMR_phase(V3_pu, "Δ-Y")
+print(f"V3_pu = {round(np.abs(V3_pu),3)} < {round(np.angle(V3_pu, deg=True),3)}° pu")
+
 V2_pu = pu_functions.calc_vpu("V2_pu", V3_pu, I3_pu, Z_Ln1pu)
-V1_pu = pu_functions.calc_vpu("V1_pu", V2_pu, I3_pu, x_T1pu)
+
+V1_pu = V3_pu + I3_pu * 1j * x_T1pu
+V1_pu = pu_functions.XFMR_phase(V1_pu, "Y-Δ")
+print(f"V1_pu = {round(np.abs(V1_pu),3)} < {round(np.angle(V1_pu, deg=True),3)}° pu")
+
 
 print("\nper unit currents:")
 pu_functions.calc_ipu("I6_pu", I6_pu)
@@ -241,7 +263,7 @@ pu_functions.calc_ipu("I1_pu", I3_pu)
 print('\nactual voltages:')
 V6_actual = pu_functions.calc_actualv("V6", 4.16e3, V6_pu)
 V5_actual = pu_functions.calc_actualv("V5", 12.47e3, V5_pu)
-V4_actual = pu_functions.calc_actualv("V4", 230e3, V4_pu)
+V4_actual = pu_functions.calc_actualv("V4", 12.47e3, V4_pu)
 V3_actual = pu_functions.calc_actualv("V3", 230e3, V3_pu)
 V2_actual = pu_functions.calc_actualv("V2", 230e3, V2_pu)
 V1_actual = pu_functions.calc_actualv("V1", 20e3, V1_pu)
@@ -281,9 +303,16 @@ V5_pu = pu_functions.calc_vpu("V5_pu", V6_pu, I6_pu, x_T3pu)
 V4_pu = pu_functions.calc_vpu("V4_pu", V5_pu, I6_pu, Z_Ln2pu)
 I_Ld1 = V4_pu / Z_Ld1pu
 I3_pu = I6_pu + I_Ld1
-V3_pu = pu_functions.calc_vpu("V3_pu", V4_pu, I3_pu, x_T2pu)
+
+V3_pu = V4_pu + I3_pu * 1j * x_T2pu
+V3_pu = pu_functions.XFMR_phase(V3_pu, "Δ-Y")
+print(f"V3_pu = {round(np.abs(V3_pu),3)} < {round(np.angle(V3_pu, deg=True),3)}° pu")
+
 V2_pu = pu_functions.calc_vpu("V2_pu", V3_pu, I3_pu, Z_Ln1pu)
-V1_pu = pu_functions.calc_vpu("V1_pu", V2_pu, I3_pu, x_T1pu)
+
+V1_pu = V3_pu + I3_pu * 1j * x_T1pu
+V1_pu = pu_functions.XFMR_phase(V1_pu, "Y-Δ")
+print(f"V1_pu = {round(np.abs(V1_pu),3)} < {round(np.angle(V1_pu, deg=True),3)}° pu")
 
 print("\nper unit currents:")
 pu_functions.calc_ipu("I6_pu", I6_pu)
@@ -296,16 +325,166 @@ pu_functions.calc_ipu("I1_pu", I3_pu)
 print('\nactual voltages:')
 V6_actual = pu_functions.calc_actualv("V6", 4.16e3, V6_pu)
 V5_actual = pu_functions.calc_actualv("V5", 12.47e3, V5_pu)
-V4_actual = pu_functions.calc_actualv("V4", 230e3, V4_pu)
+V4_actual = pu_functions.calc_actualv("V4", 12.47e3, V4_pu)
 V3_actual = pu_functions.calc_actualv("V3", 230e3, V3_pu)
 V2_actual = pu_functions.calc_actualv("V2", 230e3, V2_pu)
 V1_actual = pu_functions.calc_actualv("V1", 20e3, V1_pu)
 
 
 
+
+
 # ------------------ 6 ---------------------------
-# reflection on pf correction
+# reflection on pf correction (95% to 105% of rated values)
+
+# Without the pf correction solution, the voltage level errors (V6 to V1) are 3.8%, 3.1%, 7.2%, 12.4%, 13.7%, and 13.2%
+
+# with correction = 0.5, the voltage level errors (V6 to V1) are 3.8%, 3.7%, 6.9%, 9.9%, 9.4%, and 8.9%
+
+# with correction = 1, the voltage level errors (V6 to V1) are 3.8%, 3.8%, 6.1%, 6.7%, 4.4%, and 3.9%
+
+# the pf correction corrects the voltage level to just over 95%/105% the rated voltage level.
+
+
+
+
 
 
 # ------------------ 7 ---------------------------
 # changing base values
+print('\nchanging base values:')
+S_base, V_base1, V_base2, V_base4, V_base6 = pu_functions.change_base_vals(50e6,10e3,100e3,30e3,12e3)
+
+pu_functions.print_base("S_base",round(S_base/(1e6)),"MVA")
+pu_functions.print_base("V_base1",round(V_base1/(1e3)),"kV")
+pu_functions.print_base("V_base2",round(V_base2/(1e3)),"kV")
+pu_functions.print_base("V_base4",round(V_base4/(1e3),2),"kV")
+pu_functions.print_base("V_base6",round(V_base6/(1e3),2),"kV")
+
+I_base1 = S_base / (np.sqrt(3) * V_base1)
+pu_functions.print_base("I_base1",round(I_base1/(1e3),3),"kA")
+I_base2 = S_base / (np.sqrt(3) * V_base2)
+pu_functions.print_base("I_base2",round(I_base2/(1e3),3),"kA")
+I_base4 = S_base / (np.sqrt(3) * V_base4)
+pu_functions.print_base("I_base4",round(I_base4/(1e3),3),"kA")
+I_base6 = S_base / (np.sqrt(3) * V_base6)
+pu_functions.print_base("I_base6",round(I_base6/(1e3),3),"kA")
+
+Z_base2 = V_base2**2/(S_base)
+pu_functions.print_base("Z_base2",round(Z_base2),"ohms")
+Z_base4 = V_base4**2/(S_base)
+pu_functions.print_base("Z_base4",round(Z_base4,3),"ohms")
+Z_base6 = V_base6**2/(S_base)
+pu_functions.print_base("Z_base6",round(Z_base6,3),"ohms")
+
+# transformer per unit values (given: 0.1pu)
+x_T1pu = 0.1*(S_base/100e6)
+pu_functions.print_base("Xpu_T1",x_T1pu,"pu")
+x_T2pu = 0.1*(S_base/75e6)
+pu_functions.print_base("Xpu_T2",round(x_T2pu,2),"pu")
+x_T3pu = 0.1*(S_base/25e6)
+pu_functions.print_base("Xpu_T3",x_T3pu,"pu")
+
+# line per unit values (given)
+R_Ln1, X_Ln1 = 0.0376, 0.5277
+R_Ln2, X_Ln2 = 0.4576, 1.0780
+
+len1 = 100 # miles
+len2 = 2000/5280 # ft to miles
+R_Ln1pu = (R_Ln1*len1)/Z_base2
+X_Ln1pu = (X_Ln1*len1)/Z_base2
+Z_Ln1pu = (R_Ln1 + 1j*X_Ln1)*len1/Z_base2
+pu_functions.print_zpu("Zpu_Line1",round(R_Ln1pu,3),round(X_Ln1pu,3),"pu")
+
+R_Ln2pu = (R_Ln2*len2)/Z_base4
+X_Ln2pu = (X_Ln2*len2)/Z_base4
+Z_Ln2pu = (R_Ln2 + 1j*X_Ln2)*len2/Z_base4
+pu_functions.print_zpu("Zpu_Line2",round(R_Ln2pu,3),round(X_Ln2pu,3),"pu")
+
+# load per unit values Z_Load = V^2/S < (cos^-1(pf))
+
+# Load 1 
+# given values
+S_Ld1 = 50e6 # 50MVA
+pf_Ld1 = 0.85
+
+# find load 1 per unit impedance
+phase_Ld1 = np.arccos(pf_Ld1)
+re_Ld1 = np.power(V_base4,2)/(S_Ld1) * np.cos(phase_Ld1)
+im_Ld1 = np.power(V_base4,2)/(S_Ld1) * np.sin(phase_Ld1)
+Z_Ld1 = re_Ld1 + 1j*im_Ld1
+Z_Ld1pu = Z_Ld1/Z_base4
+pu_functions.print_zpu("Zpu_Load1",round(re_Ld1,3),round(im_Ld1,3),"pu")
+
+# Load 2
+# given values
+S_Ld2 = 20e6 # 20MVA
+pf_Ld2 = 0.9
+
+# find load 2 per unit impedance
+phase_Ld2 = np.arccos(pf_Ld2)
+re_Ld2 = np.power(V_base6,2)/S_Ld2 * np.cos(phase_Ld2)
+im_Ld2 = np.power(V_base6,2)/S_Ld2 * np.sin(phase_Ld2)
+Z_Ld2 = re_Ld2 + 1j*im_Ld2
+Z_Ld2pu = Z_Ld2/Z_base6
+pu_functions.print_zpu("Zpu_Load2",round(re_Ld2,3),round(im_Ld2,3),"pu\n")
+
+
+# ---------------------------- 3 & 4 for new base values -----------------------------------
+# assume 4kV operating voltage at Load 2
+print("3.")
+# Bus 6
+V6_actual = V_base6
+V6_pu = V6_actual/V_base6
+print("per unit voltages:")
+print(f"V6_pu = {round(V6_pu,3)} < 0 pu")
+
+I6_pu = V6_pu/Z_Ld2pu
+
+# other buses
+V5_pu = pu_functions.calc_vpu("V5_pu", V6_pu, I6_pu, x_T3pu)
+V4_pu = pu_functions.calc_vpu("V4_pu", V5_pu, I6_pu, Z_Ln2pu)
+
+I_Ld1 = V4_pu/Z_Ld1pu
+I3_pu = I6_pu + I_Ld1
+
+V3_pu = V4_pu + I3_pu * 1j * x_T2pu
+V3_pu = pu_functions.XFMR_phase(V3_pu, "Δ-Y")
+print(f"V3_pu = {round(np.abs(V3_pu),3)} < {round(np.angle(V3_pu, deg=True),3)}° pu")
+
+V2_pu = pu_functions.calc_vpu("V2_pu", V3_pu, I3_pu, Z_Ln1pu)
+
+V1_pu = V3_pu + I3_pu * 1j * x_T1pu
+V1_pu = pu_functions.XFMR_phase(V1_pu, "Y-Δ")
+print(f"V1_pu = {round(np.abs(V1_pu),3)} < {round(np.angle(V1_pu, deg=True),3)}° pu")
+
+print("per unit currents:")
+pu_functions.calc_ipu("I6_pu",I6_pu)
+pu_functions.calc_ipu("I5_pu",I6_pu)
+pu_functions.calc_ipu("I4_pu",I6_pu)
+pu_functions.calc_ipu("I3_pu",I3_pu)
+pu_functions.calc_ipu("I2_pu",I3_pu)
+pu_functions.calc_ipu("I1_pu",I3_pu)
+
+# calculate actual voltages
+print ('\n4.')
+V6_actual = pu_functions.calc_actualv("V6",V_base6,V6_pu)
+V5_actual = pu_functions.calc_actualv("V5",V_base4,V5_pu)
+V4_actual = pu_functions.calc_actualv("V4",V_base4,V4_pu)
+V3_actual = pu_functions.calc_actualv("V3",V_base2,V3_pu)
+V2_actual = pu_functions.calc_actualv("V2",V_base2,V2_pu)
+V1_actual = pu_functions.calc_actualv("V1",V_base1,V1_pu)
+
+# calculate actual currents
+print('')
+I6_actual = pu_functions.calc_actuali("I6",I_base6, I6_pu)
+I5_actual = pu_functions.calc_actuali("I5",I_base4, I6_pu)
+I4_actual = pu_functions.calc_actuali("I4",I_base4, I6_pu)
+I3_actual = pu_functions.calc_actuali("I3",I_base2, I3_pu)
+I2_actual = pu_functions.calc_actuali("I2",I_base2, I3_pu)
+I1_actual = pu_functions.calc_actuali("I1",I_base1, I3_pu)
+
+
+
+# reflection on changing base values:
+# the base values stay the same but the per unit values change
--- power/pu_functions.py
+++ power/pu_functions.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cmath
+
+deg = np.pi/180
 
 def print_base(base_name, base_val, units):
     print(f"{base_name} = {base_val} {units}")
@@ -8,20 +11,20 @@ def print_zpu(z_name, r_val, x_val, units):
 
 def calc_vpu(v_name, v_pu, i_pu, z_pu):
     vpu = v_pu + i_pu * z_pu
-    print(f"{v_name} = {round(np.abs(vpu),3)} < {round(np.angle(vpu),3)} pu")
+    print(f"{v_name} = {round(np.abs(vpu),3)} < {round(np.angle(vpu,deg=True),3)}° pu")
     return vpu
 
 def calc_ipu(i_name,i_pu):
-    print(f"{i_name} = {round(np.abs(i_pu),3)} < {round(np.angle(i_pu),3)} pu")
+    print(f"{i_name} = {round(np.abs(i_pu),3)} < {round(np.angle(i_pu,deg=True),3)}° pu")
 
 def calc_actualv(v_name,v_level,Vpu):
     v_actual = v_level * Vpu
-    print(f"{v_name} = {round(np.abs(v_actual/1000),3)} < {round(np.angle(v_actual),3)} kV")
+    print(f"{v_name} = {round(np.abs(v_actual/1000),3)} < {round(np.angle(v_actual, deg=True),3)}° kV")
     return v_actual
 
 def calc_actuali(I_name,I_base,Ipu):
     I_actual = I_base * Ipu
-    print(f"{I_name} = {round(np.abs(I_actual/1000),3)} < {round(np.angle(I_actual),3)} kA")
+    print(f"{I_name} = {round(np.abs(I_actual/1000),3)} < {round(np.angle(I_actual, deg=True),3)}° kA")
     return I_actual
 
 def pf_correction(pf_init, correction):
@@ -34,3 +37,19 @@ def pf_correction(pf_init, correction):
     pf_corrected = np.cos(pfangle_target)
 
     return pf_corrected
+
+def XFMR_phase(Vpu, connection):
+    if connection == "Y-Δ":
+        angle = 30
+    elif connection == "Δ-Y":
+        angle = -30
+    return Vpu * cmath.exp(1j * angle * deg)
+
+def change_base_vals(S_base_new, V_base1_new, V_base2_new, V_base4_new, V_base6_new):
+    S_base = S_base_new
+    V_base1 = V_base1_new
+    V_base2 = V_base2_new
+    V_base4 = V_base4_new
+    V_base6 = V_base6_new
+    return S_base, V_base1, V_base2, V_base4, V_base6
+
